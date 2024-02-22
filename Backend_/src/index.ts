53975d60c5fcd728d4135fdcdc5447ad66b03b7e
SaumyaSingh1 <rajpootsaumya1211@gmail.com>
2024-02-22 15:32:06 +0530

    adding Refresh and access token in the signup feature

--- Backend_/src/index.ts
+++ Backend_/src/index.ts
@@ -1,10 +1,35 @@
 import express, { Request, Response } from 'express';
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import query from './db/database'; // Import the database query function
 import router from './routes/authRoutes'; // Import the router for authentication routes
 import bodyParser from 'body-parser'; // Import bodyParser to parse request bodies
+import * as dotenv from "dotenv" 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Load environment variables from a .env file
+dotenv.config();
+// Enable CORS middleware with credentials support
+app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  );
+  
+  // Parse incoming JSON requests with a size limit of 16kb
+  app.use(express.json());
+  
+  // Parse incoming URL-encoded requests with a size limit of 16kb
+  app.use(express.urlencoded({ extended: true}));
+  
+  // Serve static files from the "public" directory
+  app.use(express.static("public"));
+  
+  // Parse cookies using cookie-parser middleware
+  app.use(cookieParser());
+  
 // Middleware to parse JSON bodies without it body of incoming data from client can't be read by server 
 app.use(bodyParser.json());
 
--- Backend_/src/routes/authRoutes.ts
+++ Backend_/src/routes/authRoutes.ts
@@ -1,39 +1,62 @@
-import { Router,Request,Response } from "express";
-import customer from '../models/customer'
-import query from "../db/database";
-import bcrypt from 'bcrypt'
-const router=Router();
-//signup route
-router.post('/signup',async (req:Request,res:Response)=>{
-   try{ let { name,email, password,contact,address}:customer=req.body;
-    console.log(req.body);
-    if(!name || !email || !password){
-        return res.status(400).json({message:"Please provide name, email, and password"})
+import { Router, Request, Response } from 'express';
+// import cookieParser from 'cookie-parser';
+import customer from '../models/customer';
+import query from '../db/database';
+import bcrypt from 'bcrypt';
+import { generateAccessToken, generateRefreshToken } from '../utils/tokenUtils';
+
+const router = Router();
+// router.use(cookieParser());
+console.log("refresh token : ",process.env.REFRESH_TOKEN_SECRET);
+
+
+// signup route
+router.post('/signup', async (req: Request, res: Response) => {
+    try {
+        let { name, email, password, contact, address }: customer = req.body;
+        console.log(req.body);
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: 'Please provide name, email, and password' });
+        }
+
+        // Trim leading and trailing whitespaces
+        email = email.trim();
+        password = password.trim();
+
+        // if user already exist
+        const emailExist = await query<customer>('SELECT * FROM customer WHERE email=$1', [email]);
+
+        if (emailExist.rows.length > 0) {
+            return res.status(400).json({ message: 'Email already exists' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // INSERT NEW ENTRY
+        const result = await query<customer>(
+            'INSERT INTO customer(name, email, password,contact,address) VALUES($1,$2,$3,$4,$5) RETURNING user_id',
+            [name, email, hashedPassword, contact, address]
+        );
+
+        // MAKING REFRESH TOKEN
+        const accessToken = generateAccessToken({ user_id: result.rows[0].user_id, email });
+        const refreshToken = generateRefreshToken({ user_id: result.rows[0].user_id, email });
+
+
+
+        // STORE REFRESH TOKEN IN DATABASE
+        await query<customer>('UPDATE customer SET refresh_token =$1 WHERE user_id=$2', [refreshToken, result.rows[0].user_id]);
+
+        // SEND TOKEN AS COOKIES
+        res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, sameSite: 'strict' });
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'strict' });
+
+        res.status(201).json({ message: 'User Registered successfully', userId: result.rows[0].user_id, accessToken, refreshToken });
+    } catch (error:any) {
+        // Handle errors during sign-up
+        console.error('Error during sign-up:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    // Trim leading and trailing whitespaces
-     email = email.trim();
-     password = password.trim();
-
-    //if user already exist
-     const emailExist= await query<customer>('SELECT * FROM customer WHERE email=$1',[email])
-
-     if (emailExist.rows.length > 0) {
-        return res.status(400).json({ message: "Email already exists" });
-    }
-
-     const hashedPassword = await bcrypt.hash(password, 10);
-
-     //INSERT NEW ENTRY 
-    const result=await query<customer>('INSERT INTO customer(name, email, password,contact,address) VALUES($1,$2,$3,$4,$5) RETURNING user_id',[ name,email, hashedPassword,contact,address])
-
-    res.status(201).json({ message: "User created successfully", userId: result.rows[0].user_id });
-}
-catch(error:any){
-    // Handle errors during sign-up
-    console.error('Error during sign-up:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-}
-})
+});
 
 export default router;
