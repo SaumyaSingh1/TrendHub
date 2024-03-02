6477a7b349d5e7a37cee44d16a8188688d94db23
SaumyaSingh1 <rajpootsaumya1211@gmail.com>
2024-03-02 21:20:13 +0530

    comments added in app page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -12,21 +12,36 @@ import AddToCart from './component/AddToCart';
 import UserAccount from './component/UserAccount';
 import Orders from './component/Orders';
 import OrderConfirmation from './component/OrderConfirmation';
+
 function App() {
   return (
+    // Router component to enable routing
     <Router>
+      {/* Provider component to provide product context to all components */}
       <ProductProvider>
+        {/* Routes component to define various routes */}
         <Routes>
+          {/* Layout component for common layout */}
           <Route path="/" element={<Layout/>}>
+            {/* Route for login page */}
             <Route path="/login" element={<Login/>} />
+            {/* Route for signup page */}
             <Route path="/signup" element={<SignupForm/>} />
+            {/* Route for home page */}
             <Route path="/" element={<HomePage />} />
+            {/* Route for inventory page */}
             <Route path="/inventory/*" element={<Inventory />} />
+            {/* Route for product details page */}
             <Route path="/details/:productId" element={<Details />}/>
+            {/* Route for wishlist page */}
             <Route path="/wishlist" element={<Wishlist />} />
+            {/* Route for adding items to cart */}
             <Route path="/addtocart" element={<AddToCart />} />
+            {/* Route for user account page */}
             <Route path="/userAccount" element={<UserAccount />} />
+            {/* Route for viewing orders */}
             <Route path="/orders" element={<Orders />} />
+            {/* Route for order confirmation page */}
             <Route path="/OrderConfirmation" element={<OrderConfirmation/>}/>
           </Route>
         </Routes>
